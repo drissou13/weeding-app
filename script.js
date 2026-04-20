75f6cfdc9025e90040658f603f4761504ca07ea2
dbprod <driss.boukhamla+1@gmail.com>
2026-04-20 13:14:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbGR-FuQVWt-LgiqbWWQY2MRmxgtFRPSQ",
+  authDomain: "wedding-app-e188a.firebaseapp.com",
+  databaseURL: "https://wedding-app-e188a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wedding-app-e188a",
+  storageBucket: "wedding-app-e188a.firebasestorage.app",
+  messagingSenderId: "419917976315",
+  appId: "1:419917976315:web:95d3024d4601ffe824565e",
+  measurementId: "G-12VG1G5WV0"
+};
